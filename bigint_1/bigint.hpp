0697fd369f9a82188c120b29ccfb5dc1ff425c14
Patricia Manzanas Tomas <pamanzan@student.42barcelona.com>
2026-05-12 17:44:10 +0200

    started bigint_1

---
+++ bigint_1/bigint.hpp
@@ -0,0 +1,22 @@
+#ifndef HTTP_BIGINT
+#define HTTP_BIGINT
+
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+class bigint
+{
+private:
+    std::string _str;
+
+public:
+    bigint();
+    bigint(unsigned int num);
+    bigint(const bigint &obj);
+    bigint operator=(const bigint &obj);
+    std::string getStr() const;
+};
+
+#endif
