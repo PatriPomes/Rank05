9a6498ddcf058ea4e9f6648fbaa58632df4c53dd
Patricia Manzanas Tomas <pamanzan@student.42barcelona.com>
2026-05-31 13:05:47 +0200

    bigint_5

--- bigint_5/bigint.cpp
+++ bigint_5/bigint.cpp
@@ -32,3 +32,142 @@ std::ostream &operator<<(std::ostream &os, const bigint &obj)
     os << obj.getStr();
     return os;
 }
+
+bigint &bigint::operator+=(const bigint &obj)
+{
+    std::string digit_a = _str;
+    std::string digit_b = obj._str;
+
+    while (digit_a.length() < digit_b.length())
+        digit_a = "0" + digit_a;
+    while (digit_a.length() > digit_b.length())
+        digit_b = "0" + digit_b;
+
+    int carry = 0;
+    std::string res = "";
+    int cont = digit_a.length() - 1;
+    while (cont >= 0)
+    {
+        int sum = (digit_a[cont] - '0') + (digit_b[cont] - '0') + carry;
+        res = char(sum % 10 + '0') + res;
+        carry = sum / 10;
+        cont--;
+    }
+    if (carry > 0)
+        res = char(carry % 10 + '0') + res;
+    _str = res;
+    return *this;
+}
+
+bigint bigint::operator+(const bigint &obj) const
+{
+    bigint tmp(*this);
+    tmp += obj;
+    return tmp;
+}
+
+bigint bigint::operator++()
+{
+    bigint plus(1);
+    *this += plus;
+    return *this;
+}
+
+bigint bigint::operator++(int)
+{
+    bigint plus(1);
+    bigint tmp(*this);
+    *this += plus;
+    return tmp;
+}
+
+bigint bigint::operator<<(unsigned int num) const
+{
+    bigint tmp(*this);
+    tmp._str.insert(tmp._str.end(), num, '0');
+    return tmp;
+}
+
+bigint bigint::operator>>(unsigned int num) const
+{
+    bigint tmp(*this);
+    if (num >= tmp._str.length())
+        tmp._str = "0";
+    else
+        tmp._str.erase(tmp._str.length() - num, num);
+    return tmp;
+}
+
+bigint &bigint::operator<<=(unsigned int num)
+{
+    (*this) = (*this) << num;
+    return *this;
+}
+bigint &bigint::operator>>=(unsigned int num)
+{
+    (*this) = (*this) >> num;
+    return *this;
+}
+
+unsigned int stringToUnit(std::string source)
+{
+    std::stringstream ss(source);
+    unsigned int num;
+    ss >> num;
+    return num;
+}
+
+bigint bigint::operator<<(const bigint &obj) const
+{
+    bigint tmp(*this);
+    tmp = tmp << stringToUnit(obj._str);
+    return tmp;
+}
+
+bigint bigint::operator>>(const bigint &obj) const
+{
+    bigint tmp(*this);
+    tmp = tmp >> stringToUnit(obj._str);
+    return tmp;
+}
+
+bigint &bigint::operator<<=(const bigint &obj)
+{
+    (*this) = (*this) <<= stringToUnit(obj._str);
+    return (*this);
+}
+
+bigint &bigint::operator>>=(const bigint &obj)
+{
+    (*this) = (*this) >>= stringToUnit(obj._str);
+    return (*this);
+}
+
+bool bigint::operator<(const bigint &obj) const
+{
+    size_t len_a = _str.length();
+    size_t len_b = obj._str.length();
+    if (len_a != len_b)
+        return (len_a < len_b);
+    return (_str < obj._str);
+}
+bool bigint::operator>(const bigint &obj) const
+{
+    return (obj < *this);
+}
+bool bigint::operator<=(const bigint &obj) const
+{
+    return (*this < obj || *this == obj);
+}
+bool bigint::operator>=(const bigint &obj) const
+{
+    return (*this > obj || *this == obj);
+}
+bool bigint::operator==(const bigint &obj) const
+{
+    return (_str == obj._str);
+}
+bool bigint::operator!=(const bigint &obj) const
+{
+    return (_str != obj._str);
+}
--- bigint_5/main.cpp
+++ bigint_5/main.cpp
@@ -17,55 +17,55 @@ int main(void)
     std::cout << "e = " << e << std::endl;
     std::cout << std::endl;
 
-    // std::cout << YELLOW << "++++ ADD OBJECTS ++++" << RESET << std::endl;
+    std::cout << YELLOW << "++++ ADD OBJECTS ++++" << RESET << std::endl;
 
-    // std::cout << "a + b = " << a + b << std::endl; //63
-    // std::cout << "a + c = " << a + c << std::endl; //42
-    // std::cout << "(c += a) = " << (c += a) << std::endl; //42
-    // std::cout << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;       // 63
+    std::cout << "a + c = " << a + c << std::endl;       // 42
+    std::cout << "(c += a) = " << (c += a) << std::endl; // 42
+    std::cout << std::endl;
 
-    // std::cout << BLUE << "++++ INCREMENT OBJECTS ++++" << RESET << std::endl;
-    // std::cout << "b = " << b << std::endl; //21
-    // std::cout << "++b = " << ++b << std::endl; //22
-    // std::cout << "b++ = " << b++ << std::endl; //22
-    // std::cout << "b = " << b << std::endl; //23
-    // std::cout << std::endl;
+    std::cout << BLUE << "++++ INCREMENT OBJECTS ++++" << RESET << std::endl;
+    std::cout << "b = " << b << std::endl;     // 21
+    std::cout << "++b = " << ++b << std::endl; // 22
+    std::cout << "b++ = " << b++ << std::endl; // 22
+    std::cout << "b = " << b << std::endl;     // 23
+    std::cout << std::endl;
 
-    // std::cout << YELLOW << "++++ << OPERAND ++++" << RESET << std::endl;
-    // // b = 23, b << 10 -> 23000000000 + 42 = 23000000042
-    // std::cout << "(b << 10): " << (b << 10) << std::endl;
-    // std::cout << "(b << 10) + 42 = " << ((b << 10) + 42) << std::endl;
+    std::cout << YELLOW << "++++ << OPERAND ++++" << RESET << std::endl;
+    // b = 23, b << 10 -> 23000000000 + 42 = 23000000042
+    std::cout << "(b << 10): " << (b << 10) << std::endl;
+    std::cout << "(b << 10) + 42 = " << ((b << 10) + 42) << std::endl;
 
-    // std::cout << std::endl;
+    std::cout << std::endl;
 
-    // std::cout << BLUE << "++++ <<= OBJECTS ++++" << RESET << std::endl;
-    // std::cout << "(d <<= 4) = " << (d <<= 4) << ", d: " << d << std::endl;
-    // std::cout << "(d >>= 2) = " << (d >>= (const bigint)2) << ", d: " << d << std::endl;
-    // std::cout << std::endl;
+    std::cout << BLUE << "++++ <<= OBJECTS ++++" << RESET << std::endl;
+    std::cout << "(d <<= 4) = " << (d <<= 4) << ", d: " << d << std::endl;
+    std::cout << "(d >>= 2) = " << (d >>= (const bigint)2) << ", d: " << d << std::endl;
+    std::cout << std::endl;
 
-    // std::cout << YELLOW << "++++ = OPERAND ++++" << RESET << std::endl;
-    // std::cout << "a = " << a << std::endl; // a = 42
-    // std::cout << "d = " << d << std::endl; // d = 133700
-    // std::cout << std::endl;
+    std::cout << YELLOW << "++++ = OPERAND ++++" << RESET << std::endl;
+    std::cout << "a = " << a << std::endl; // a = 42
+    std::cout << "d = " << d << std::endl; // d = 133700
+    std::cout << std::endl;
 
-    // std::cout << BLUE << "++++ OTHER OPERANDS ++++" << RESET << std::endl;
-    // std::cout << "(d < a) = " << (d < a) << std::endl;   // (d < a) = 0
-    // std::cout << "(d > a) = " << (d > a) << std::endl;   // (d > a) = 1
-    // std::cout << "(d == d) = " << (d == d) << std::endl; // (d == d) = 1
-    // std::cout << "(d != a) = " << (d != a) << std::endl; // (d != a) = 1
-    // std::cout << "(d <= a) = " << (d <= a) << std::endl; // (d <= a) = 0
-    // std::cout << "(d >= a) = " << (d >= a) << std::endl; // (d >= a) = 1
-    // std::cout << std::endl;
+    std::cout << BLUE << "++++ OTHER OPERANDS ++++" << RESET << std::endl;
+    std::cout << "(d < a) = " << (d < a) << std::endl;   // (d < a) = 0
+    std::cout << "(d > a) = " << (d > a) << std::endl;   // (d > a) = 1
+    std::cout << "(d == d) = " << (d == d) << std::endl; // (d == d) = 1
+    std::cout << "(d != a) = " << (d != a) << std::endl; // (d != a) = 1
+    std::cout << "(d <= a) = " << (d <= a) << std::endl; // (d <= a) = 0
+    std::cout << "(d >= a) = " << (d >= a) << std::endl; // (d >= a) = 1
+    std::cout << std::endl;
 
-    // std::cout << YELLOW << "++++ EXTRA ++++" << RESET << std::endl;
-    // bigint x(12345678);
-    // bigint y(5);
-    // std::cout << "(x << y) = " << (x << y) << ", x: " << x << ", y: " << y << std::endl;
-    // std::cout << "(x >>= y) = " << (x >>= y) << ", x: " << x << ", y: " << y << std::endl;
-    // std::cout << "(x >= y) = " << (x >= y) << ", x: " << x << ", y: " << y << std::endl;
+    std::cout << YELLOW << "++++ EXTRA ++++" << RESET << std::endl;
+    bigint x(12345678);
+    bigint y(5);
+    std::cout << "(x << y) = " << (x << y) << ", x: " << x << ", y: " << y << std::endl;
+    std::cout << "(x >>= y) = " << (x >>= y) << ", x: " << x << ", y: " << y << std::endl;
+    std::cout << "(x >= y) = " << (x >= y) << ", x: " << x << ", y: " << y << std::endl;
 
-    // std::cout << "x= " << (x <<= 5) << ", y= " << (y <<= 12) << std::endl;
-    // std::cout << "(x >= y) = " << (x >= y) << ", x: " << x << ", y: " << y << std::endl;
+    std::cout << "x= " << (x <<= 5) << ", y= " << (y <<= 12) << std::endl;
+    std::cout << "(x >= y) = " << (x >= y) << ", x: " << x << ", y: " << y << std::endl;
 
     return (0);
 }
